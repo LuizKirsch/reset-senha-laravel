b36748e5718614a74e27bfac4bd976ea8b6d3fb3
lluiziull <luizkirsch0@gmail.com>
2024-08-13 11:24:17 -0300

    feat: add notification

---
+++ app/Notifications/PasswordResetNotification.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordResetNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public $url
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->markdown('vendor.notifications.email')
+                    ->subject('Noticficação de troca de senha')
+                    ->greeting('Olá!')
+                    ->salutation('Atenciosamente, Equipe ' . config('app.name'))
+                    ->from('teste@teste.com')
+                    ->line('Link para alteração.')
+                    ->action('Alterar Senha', url($this->url))
+                    ->line('Obrigado por usar o '. config('app.name'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
