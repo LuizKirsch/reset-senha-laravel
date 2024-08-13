814443ba550f66fda0c97ac3e62ad1d482e3a983
lluiziull <luizkirsch0@gmail.com>
2024-08-13 11:25:55 -0300

    feat: readme e outras alterações

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Auth\Notifications\ResetPassword;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // ResetPassword::createUrlUsing(function (User $user, string $token) {
+        //     return URL::to('/reset-password/' . $token);
+        // });
     }
 }
