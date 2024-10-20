8437b997006b476782ffa3f05d900cf64078c32f
lluiziull <luizkirsch0@gmail.com>
2024-10-20 18:40:21 -0300

    fix: alterada as mensagens de confirmação de envio de email

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,8 +47,10 @@ class AuthController extends Controller
         );
 
         return $status === Password::RESET_LINK_SENT
-            ? back()->with(['status' => __($status)])
-            : back()->withErrors(['email' => __($status)]);
+            // ? back()->with(['status' => __($status)])
+            ? back()->with(['status' => 'Enviamos o e-mail para redefinição!'])
+            // : back()->withErrors(['email' => __($status)]);
+            : back()->withErrors(['email' => 'Aconteceu algum erro!']);
     }
 
     // exibe o formulário de redefinição de senha
